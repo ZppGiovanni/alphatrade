83fad30ba37e584051f59b37cc29d393fc9714aa
matteriandrea30-pixel <matteriandrea30@gmail.com>
2026-04-28 11:26:25 +0200

    feat: add MACDCrossoverStrategy with tests

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -1,5 +1,11 @@
 from strategies.base import Strategy
 from strategies.mean_reversion import MeanReversionStrategy
 from strategies.momentum import MomentumStrategy
+from strategies.macd_crossover import MACDCrossoverStrategy
 
-__all__ = ["Strategy", "MeanReversionStrategy", "MomentumStrategy"]
+__all__ = [
+    "Strategy",
+    "MeanReversionStrategy",
+    "MomentumStrategy",
+    "MACDCrossoverStrategy",
+]
